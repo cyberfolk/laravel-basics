808cf9dc0a4e1403872317eb9437aa8d8ef6909d
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-24 15:39:00 +0200

    Set layout

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,15 @@
+    <header id="app_header" class="text-center pb-4">
+        <nav class="navbar navbar-expand navbar-light bg-light py-0">
+            <div class="container">
+                <ul class="navbar-nav ms-auto mt-2">
+{{--                     <li class="nav-item {{ Route::currentRouteName() === 'home' ? 'text-warning fw-bold' : ''}}" href="{{ route('home')}}">Home</li>
+                    <li class="nav-item {{ Route::currentRouteName() === 'about' ? 'text-warning fw-bold' : ''}}" href="{{ route('about')}}">about</li>
+                    <li class="nav-item {{ Route::currentRouteName() === 'movies' ? 'text-warning fw-bold' : ''}}" href="{{ route('movies')}}">movies</li> --}}
+                </ul>
+            </div>
+            <!-- /.container -->
+        </nav>
+        <!-- /.navbar -->
+            <h1 class="text-center text-warning"> Movie DB </h1>
+    </header>
+    <!-- /#app_header -->
