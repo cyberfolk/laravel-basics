aa74fd9d4785e4daefcaeb1692d233c4b72ec25d
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-24 17:22:07 +0200

    Add category to the lists

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -4,7 +4,7 @@
 
 <section id="movies">
     <div class="container pt-5 pb-3">
-        <div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-3 pb-4">
+        <div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-4 pb-4">
             @foreach($movies as $movie)
             <div class="col h-100">
                 <div class="movie card shadow h-100">
@@ -14,10 +14,22 @@
                     </div>
                     <!-- /.card-body -->
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item">{{ $movie->original_title }}</li>
-                        <li class="list-group-item">{{ $movie->nationality }}</li>
-                        <li class="list-group-item">{{ $movie->date }}</li>
-                        <li class="list-group-item">{{ $movie->vote }}</li>
+                        <li class="list-group-item">
+                            <span class="fw-bold">Original Titile: </span>
+                            <span>{{ $movie->original_title }}</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="fw-bold">Nationality: </span>
+                            <span>{{ $movie->nationality }}</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="fw-bold">Release date:</span>
+                            <span>{{ $movie->date }}</span>
+                        </li>
+                        <li class="list-group-item">
+                            <span class="fw-bold">Vote: </span>
+                            <span>{{ $movie->vote }}</span>
+                        </li>
                     </ul>
                     <!-- /.list-group -->
                 </div>
