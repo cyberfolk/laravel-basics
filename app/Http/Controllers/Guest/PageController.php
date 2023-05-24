f6e8f54b0af1963d40acd7b4970514a66fbe09ea
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-24 15:38:11 +0200

    Add Route

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,24 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Movie;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
 {
-    //
+    public function index()
+    {
+        return view('home');
+    }
+
+    public function movies()
+    {
+        $movies = Movie::all();
+        return view('movies', compact('movies'));
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
 }
