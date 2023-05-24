6ad3ee73a8c095f26f6973a1f96a6d6e4114ddc3
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-24 16:11:37 +0200

    Show movie data with card

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -4,13 +4,25 @@
 
 <section id="movies">
     <div class="container pt-5 pb-3">
-        <div class="row row-cols-2 row-cols-lg-6 g-2 g-lg-3 pb-4">
+        <div class="row row-cols-2 row-cols-lg-4 g-2 g-lg-3 pb-4">
             @foreach($movies as $movie)
-            <div class="col">
-                <div class="movie">
-                    <div class="title">{{ $movie->title }}</div>
+            <div class="col h-100">
+                <div class="movie card shadow h-100">
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $movie->title }}</h4>
+                    </div>
+                    <!-- /.card-body -->
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">{{ $movie->original_title }}</li>
+                        <li class="list-group-item">{{ $movie->nationality }}</li>
+                        <li class="list-group-item">{{ $movie->date }}</li>
+                        <li class="list-group-item">{{ $movie->vote }}</li>
+                    </ul>
+                    <!-- /.list-group -->
                 </div>
+                <!-- /.card -->
             </div>
+            <!-- /.col -->
             @endforeach
         </div>
         <!-- /.row -->
