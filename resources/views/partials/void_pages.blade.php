85b778f05410017fb7f6edde755202998b885f4d
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-24 17:22:21 +0200

    Translate to ENG

--- resources/views/partials/void_pages.blade.php
+++ resources/views/partials/void_pages.blade.php
@@ -1,11 +1,11 @@
 <section id="void_page" class="py-5 text-center">
     <div class="alert alert-danger" role="alert">
         <h3 class="text-uppercase fw-bold"> !!! {{Route::currentRouteName()}} !!! </h3> 
-        <h4>Al momento è vuota</h4>
-        <h3>
-            <span>Vai a</span>
+        <h4>It's currently empty</h4>
+        <h4>
+            <span>Go to</span>
             <a href="{{route('movies')}}">Movies</a>
-        </h3>
+        </h4>
     </div>
 </section> 
 <!-- /#void_page -->
