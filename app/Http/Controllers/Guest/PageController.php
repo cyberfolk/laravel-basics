779f853ef3715dd5dc33ecfb0b2896dbf9eb0fb8
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-25 15:25:03 +0200

    Add Route and PageController

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -3,9 +3,25 @@
 namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
+use App\Models\Train;
 use Illuminate\Http\Request;
+
 
 class PageController extends Controller
 {
-    //
+    public function home()
+    {
+        return view('home');
+    }
+
+    public function movies()
+    {
+        $trains = Train::all();
+        return view('trains', compact('trains'));
+    }
+
+    public function about()
+    {
+        return view('about');
+    }
 }
