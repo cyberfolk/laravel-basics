736db5a0d90bfdc492b054b3348026c5a5d67c0f
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-05-25 16:00:54 +0200

    Do migrations create_trains_table

--- database/migrations/2023_05_25_125709_create_trains_table.php
+++ database/migrations/2023_05_25_125709_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 50);
+            $table->string('departure_station', 50);
+            $table->string('arrival_station', 50);
+            $table->time('time_station');
+            $table->time('time_arrival');
+            $table->string('train_code', 10);
+            $table->tinyInteger('carriage')->unsigned();
+            $table->boolean('in_time')->default(true);
+            $table->boolean('cancelled')->default(false);
             $table->timestamps();
         });
     }
